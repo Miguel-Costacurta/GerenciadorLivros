7b2f40e383159189d72b7e42d1e8320f4eb5d823
Miguel-Costacurta <miguelfcostacurta@gmail.com>
2026-03-17 19:24:20 -0300

    Produto Repositorio Criado

---
+++ books/src/main/java/com/books/books/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.books.books.repositories;
+
+import com.books.books.models.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long > {
+
+}
