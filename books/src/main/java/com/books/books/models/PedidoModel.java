bae43180081b626a253f9a1c0240e58315669690
Miguel-Costacurta <miguelfcostacurta@gmail.com>
2026-03-18 10:20:13 -0300

    Model Pedido

---
+++ books/src/main/java/com/books/books/models/PedidoModel.java
@@ -0,0 +1,55 @@
+package com.books.books.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@RequestMapping("/pedido")
+public class PedidoModel {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "dataPedido")
+    private LocalDateTime data;
+    @Column(name = "valorTotalPedido")
+    private BigDecimal valorTotal;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
