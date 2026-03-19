49654dc74c9c6e0a5c8446cb8de5a153835c0ade
miguel-costacurta <miguelfcostacurta@gmail.com>
2026-03-18 21:33:31 -0300

    Categoria Repository

---
+++ books/src/main/java/com/books/books/repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.books.books.repositories;
+
+import com.books.books.models.CategoriasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<CategoriasModel, Long> {
+}
