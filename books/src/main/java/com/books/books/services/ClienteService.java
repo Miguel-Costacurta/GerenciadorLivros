71287b735872e8364f149d55342adb877e52a3d6
Matheus Goulart <goulartmatheus611@gmail.com>
2026-03-19 10:07:52 -0300

    Cliente Service

---
+++ books/src/main/java/com/books/books/services/ClienteService.java
@@ -0,0 +1,31 @@
+package com.books.books.services;
+
+import com.books.books.models.CategoriaModel;
+import com.books.books.models.ClienteModel;
+import com.books.books.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<ClienteModel> findAll(){ return clienteRepository.findAll();}
+    public Optional<ClienteModel> findById(Long id) {
+        return clienteRepository.findById(id);
+    }
+    public ClienteModel criarCliente(ClienteModel cliente) { return clienteRepository.save(cliente); }
+    public ClienteModel atualizarCliente(ClienteModel cliente, Long id) {
+        ClienteModel clienteAtualizado = clienteRepository.findById(id).get();
+        clienteAtualizado.setNome(cliente.getNome());
+        return clienteRepository.save(clienteAtualizado);
+    }
+    public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
