2826c517ce4b4f59bca8fc07e84d9cb440b33548
miguel-costacurta <miguelfcostacurta@gmail.com>
2026-03-18 21:20:21 -0300

    Autor Service

--- books/src/main/java/com/books/books/repositories/AutoresRepository.java
+++ books/src/main/java/com/books/books/repositories/AutoresRepository.java
@@ -2,6 +2,8 @@ package com.books.books.repositories;
 
 import com.books.books.models.AutoresModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AutoresRepository extends JpaRepository<Long, AutoresModel> {
+@Repository
+public interface AutoresRepository extends JpaRepository<AutoresModel, Long> {
 }
---
+++ books/src/main/java/com/books/books/services/AutoresService.java
@@ -0,0 +1,35 @@
+package com.books.books.services;
+
+import com.books.books.models.AutoresModel;
+import com.books.books.repositories.AutoresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AutoresService {
+
+    @Autowired
+    private AutoresRepository autoresRepository;
+
+    public List<AutoresModel> findAll() {return autoresRepository.findAll();}
+
+    public AutoresModel criarAutor(AutoresModel autoresModel) {
+        return autoresRepository.save(autoresModel);
+    }
+
+    public Optional<AutoresModel> buscarId(Long id){return autoresRepository.findById(id);}
+
+    public AutoresModel atualizarAutor(Long id, AutoresModel autoresModel){
+        AutoresModel autor =  buscarId(id).get();
+        autor.setNome(autoresModel.getNome());
+
+        return autoresRepository.save(autor);
+    }
+
+    public void deletarAutor(Long id) {
+        autoresRepository.deleteById(id);
+    }
+}
